d32e813a2e23db41888e1c9ab7a06720ff7577f9
Lucas <lucas04@foxmail.com>
2026-04-03 10:08:32 +0800

    chore: Update compileSdkVersion to 37 and upgrade Android Gradle plugin to 8.12.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.stayup.schedule"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     buildFeatures {
